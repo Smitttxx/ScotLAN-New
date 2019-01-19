76865610c5e7770edca484a6b3fb06d7e9cafb00
scotlan <rob@scotlan.events>
2019-01-19 19:45:59 +0000

    team/header changes

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -10,10 +10,94 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div class="sl--sitecontainer--background__keyboard">
-        <div className="container">
-          <br/><br/><br/>
-          <h2 class="product-heading">About Us<span class="text-muted"></span></h2>
+      <div className="sl--sitecontainer--background__keyboard">
+        <div className="container sl-ourteam-container">
+          <h2 className="product-heading">About Us<span class="text-muted"></span></h2>
+          <div className="container">
+            <div className="row">
+              ScotLAN events provide a relatively intimate LAN experience with small numbers, creating a really welcoming, friendly and fun atmosphere. The ScotLAN team do this out of pure passion, as they love putting on these events and seeing people
+              enjoy themselves. If you’ve never been to a LAN before, why not take a chance and see how fun it can be! Our events are the perfect starting point for those new to the hobby due to their small and welcoming nature. Don’t worry, it will be a
+              great social event as well.
+              <p>
+                There is also board games element as well, so you don’t even need to bring a computer if you would prefer to enjoy a weekend or day of tabletop gaming with like-minded gamers.
+              </p>
+            </div>
+            <div className="row">
+              <div className="col-lg-3">
+                <img width="150" height="150" src="https://scotlan.events/wp-content/uploads/2018/01/Rob-150x150.png" className="attachment-thumbnail size-thumbnail" alt=""
+          srcset="https://scotlan.events/wp-content/uploads/2018/01/Rob-150x150.png 150w, https://scotlan.events/wp-content/uploads/2018/01/Rob-180x180.png 180w, https://scotlan.events/wp-content/uploads/2018/01/Rob-300x300.png 300w" sizes="(max-width: 150px) 100vw, 150px" />
+
+                </div>
+                <div className="col-lg-9">
+                <figcaption className="widget-image-caption wp-caption-text">Rob</figcaption>
+                <p>Rob is an experienced Cloud Architect with a passion for gaming. After being involved with multiple LAN’s in the UK he came up with the idea of ScotLAN, He looks after our website as well as our network infrastructure.</p>
+                <div className="social-icons">
+                  <a href="wwww.discord.com"><i class="fab fa-discord"></i> </a>
+                  <a href="wwww.steam.com"><i class="fab fa-steam"></i> </a>
+                  <a href="wwww.facebook.com"><i class="fab fa-facebook"></i> </a>
+                  <a href="wwww.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                  <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top"><i class="far fa-envelope "></i></a>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-line"></div>
+            <div className="row">
+              <div className="col-lg-3">
+                <img width="150" height="150" src="https://scotlan.events/wp-content/uploads/2018/01/David-150x150.jpg" className="attachment-thumbnail size-thumbnail" alt=""
+            srcset="https://scotlan.events/wp-content/uploads/2018/01/David-150x150.jpg 150w, https://scotlan.events/wp-content/uploads/2018/01/David-180x180.jpg 180w, https://scotlan.events/wp-content/uploads/2018/01/David-300x300.jpg 300w" sizes="(max-width: 150px) 100vw, 150px" />
+
+                </div>
+                <div className="col-lg-9">
+                <figcaption className="widget-image-caption wp-caption-text">Carvid</figcaption>
+                <p>Carvid (David) cannot be bothered writing a profile about himself but hobbies include trolling Rob, gaming and trolling Rob, I encourage everyone to participate in all of these activities.</p>
+                <div className="social-icons">
+                  <a href="wwww.discord.com"><i class="fab fa-discord"></i> </a>
+                  <a href="wwww.steam.com"><i class="fab fa-steam"></i> </a>
+                  <a href="wwww.facebook.com"><i class="fab fa-facebook"></i> </a>
+                  <a href="wwww.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top"><i class="far fa-envelope "></i></a>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-line"></div>
+            <div className="row">
+              <div className="col-lg-3">
+                <img width="150" height="150" src="https://scotlan.events/wp-content/uploads/2018/01/13769576_10209952274947389_8801454101162529606_n-150x150.jpg" className="attachment-thumbnail size-thumbnail" alt=""
+            srcset="https://scotlan.events/wp-content/uploads/2018/01/13769576_10209952274947389_8801454101162529606_n-150x150.jpg 150w, https://scotlan.events/wp-content/uploads/2018/01/13769576_10209952274947389_8801454101162529606_n-300x300.jpg 300w, https://scotlan.events/wp-content/uploads/2018/01/13769576_10209952274947389_8801454101162529606_n-180x180.jpg 180w, https://scotlan.events/wp-content/uploads/2018/01/13769576_10209952274947389_8801454101162529606_n.jpg 540w" sizes="(max-width: 150px) 100vw, 150px" />
+
+                </div>
+                <div className="col-lg-9">
+                <figcaption className="widget-image-caption wp-caption-text">Smitttxx</figcaption>
+                <p>Smitttx (Laura) front-end web developer. With a passion for Gaming and Socialising – She wanted to be involved in ScotLAN. She is responsible for the Social Media and Merchandise/Branding, she is also our qualified first aider
+                should you require healing.</p>
+                <div className="social-icons">
+                  <a href="wwww.discord.com"><i class="fab fa-discord"></i> </a>
+                  <a href="wwww.steam.com"><i class="fab fa-steam"></i> </a>
+                  <a href="wwww.facebook.com"><i class="fab fa-facebook"></i> </a>
+                  <a href="wwww.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                  <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top"><i class="far fa-envelope "></i></a>
+                </div>              </div>
+              </div>
+              <div className="gradient-line"></div>
+              <div className="row">
+
+              <div className="col-lg-3">
+                <img width="150" height="150" src="https://scotlan.events/wp-content/uploads/2018/02/Eggplant_Emoji_grande-150x150.png" className="attachment-thumbnail size-thumbnail" alt="" srcset="https://scotlan.events/wp-content/uploads/2018/02/Eggplant_Emoji_grande-150x150.png 150w, https://scotlan.events/wp-content/uploads/2018/02/Eggplant_Emoji_grande-300x300.png 300w, https://scotlan.events/wp-content/uploads/2018/02/Eggplant_Emoji_grande-180x180.png 180w, https://scotlan.events/wp-content/uploads/2018/02/Eggplant_Emoji_grande.png 600w" sizes="(max-width: 150px) 100vw, 150px" />
+
+                </div>
+                <div className="col-lg-9">
+                <figcaption className="widget-image-caption wp-caption-text">Andy</figcaption>
+                <p>Andy (Andy_M29) is not a junior software developer, he has a keen passion for all things code and all things eggplant. He’s a bit of a console peasant but we are slowly converting him to the PC masterace! We really couldn’t thank him enough,
+                he’s been key in starting ScotLAN.</p>
+                <div className="social-icons">
+                  <a href="wwww.discord.com"><i class="fab fa-discord"></i> </a>
+                  <a href="wwww.steam.com"><i class="fab fa-steam"></i> </a>
+                  <a href="wwww.facebook.com"><i class="fab fa-facebook"></i> </a>
+                  <a href="wwww.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top"><i class="far fa-envelope "></i></a>
+                </div>              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/containers/Partners/Partners.js
+++ src/containers/Partners/Partners.js
@@ -11,10 +11,56 @@ export default class Partners extends Component {
   render() {
     return (
       <div class="sl--sitecontainer--background__keyboard">
-        <div className="container">
-          <br/><br/><br/>
+        <div className="container sl--partners--container">
           <h2 class="product-heading">Partners<span class="text-muted"></span></h2>
-        </div>
+
+          <div class="row">
+              <div class="col-md-3">
+                <img src="images/partner-gtomega.png" alt="Logo NOCTUA" />
+              </div>
+              <div class="col-md-9">
+                <h3>GT Omega Racing</h3>
+                <p>Established in 2009. GT Omega Racing is one of the leading brands , specialising in products for Simulation Racing, Gaming Chairs and Accessories for use in eSports and gaming.</p>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-3">
+                <img src="images/partner-humblebundle.png" alt="Logo NOCTUA" />
+              </div>
+              <div class="col-md-9">
+                <h3>Humble Bundle</h3>
+                <p>Humble Bundle is a distribution platform selling games, ebooks, software, and other digital content. Since Humble's founding in 2010, our mission has been to support charity
+                ("Humble") while providing awesome content to customers at great prices ("Bundle"). We started by offering only game bundles, but have branched out to include an online storefront,
+                a monthly subscription service, a publishing initiative, and lots more.</p>
+                <div> </div>
+                <a target="_blank" href="https://www.humblebundle.com/monthly?partner=scotlan" title="Humble website">https://www.humblebundle.com/monthly?partner=scotlan</a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-3">
+                <img src="images/partner-ess.png" alt="Logo NOCTUA" />
+              </div>
+              <div class="col-md-9">
+                <h3>Esports Scotland</h3>
+                <p>Their goal is to support competitive gaming from the grassroots to the professional level. We aim to provide opportunities for esports talent in Scotland
+                to be scouted by professional teams through hosting and sponsoring tournaments nationwide with the focus always on Scotland.</p>
+                <a target="_blank" href="https://esportsscotland.co.uk/" title="Humble website">https://esportsscotland.co.uk/</a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-3">
+                <img src="images/partner-asrock.png" alt="Logo NOCTUA" />
+              </div>
+              <div class="col-md-9">
+                <h3>ASrock</h3>
+                <p>
+                ASRock Inc. is established in 2002, specialized in the field of motherboards. ASRock strives to build up its own brand. With the 3C design concept, “Creativity,
+                Consideration, Cost-effectiveness”, the company explores the limit of motherboards manufacturing while paying attention on the eco issue at the same time,
+                developing products with the consideration of eco-friendly concept.</p>
+                <a target="_blank" href="https://www.asrock.com/index.asp" title="Humble website">https://www.humblebundle.com/monthly?partner=scotlan</a>
+              </div>
+          </div>
+      </div>
       </div>
     );
   }
